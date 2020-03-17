484d4de37c90472cd25004145730e0fb06b0fad9
jennahamlin <jennahamlin@gmail.com>
2020-03-17 13:37:26 -0400

    added better documentation within the code and displays selection of long data frame until selection occurs

--- R/mod_displayTree.R
+++ R/mod_displayTree.R
@@ -29,25 +29,29 @@ mod_displayTree_ui <- function(id){
 #' @keywords internal
 
 mod_displayTree_server <- function(input, output, session, 
-                                   treeFile,dataFile, treeformat, align, font, numscale, node){
+                                   treeFile,dataFileCleaned, treeformat, align, font, numscale, node){
   ns <- session$ns
   
-   treeObject<-reactive({
-       tibble::as_tibble(treeFile()) #convert to tibble to access and join tree and genetic distance matrix
-   }) 
-   
-   combTandG <- reactive({
-     dplyr::rename(dataFile(), label = 1)%>%  #change column1, row1 to the id of label 
-       replace(., .=="-", 0) #replace - with a 0 within the file
-   })
-   
-   gandTS4 <- reactive({
-     dplyr::full_join(treeObject(), combTandG(), by = "label")%>% #join the treeobjectt and updated genetic distance file by label
-       treeio::as.treedata() #convert to s4 object
-   })
-   
+  #convert phylogenetic tree to tibble to join tree and genetic distance matrix
+  treeObject<-reactive({
+    tibble::as_tibble(treeFile()) 
+  }) 
+  
+  #change column1, row1 to the id of label and replace - with a 0 within the file
+  combTandG <- reactive({
+    dplyr::rename(dataFileCleaned(), label = 1)%>%  
+      replace(., .=="-", 0) 
+  })
+  
+  #join the treeobject and updated genetic distance file by label and convert to s4 object
+  gandTS4 <- reactive({
+    dplyr::full_join(treeObject(), combTandG(), by = "label")%>% 
+      treeio::as.treedata() 
+  })
+  
+  #major plotting reactive using an S4 object
   make_tree <- reactive({
-    ggtree::ggtree(gandTS4(), layout = treeformat())+ #major plotting reactive
+    ggtree::ggtree(gandTS4(), layout = treeformat())+
       ggtree::geom_tiplab(align = align(), fontface = font(), family="Helvetica") + 
       ggtree::geom_treescale(width = numscale())+
       ggtree::geom_text2(ggplot2::aes(label=label, subset=!is.na(as.numeric(label)) & label >node()), nudge_x = 0.0002)
@@ -57,21 +61,22 @@ mod_displayTree_server <- function(input, output, session,
     make_tree()
   })
   
-   dataWithSelection <- reactive({
-     brushedPoints(make_tree()$data, input$plot_brush)
-   })
-  
-  output$selectedIndivs <- renderTable({ #renderTable makes a table of values - can this be accessed 
-    ifelse(dataWithSelection()$isTip == TRUE, dataWithSelection()$label, "") 
+  dataWithSelection <- reactive({
+    brushedPoints(make_tree()$data, input$plot_brush)
   })
   
-  # gandT <-reactive({
-  #   make_tree()$data %>%
-  #     dplyr::mutate_if(is.numeric,as.character, is.factor, as.character) %>%
-  #     na.omit() %>%
-  #     dplyr::select(-c(parent, node, branch.length, isTip, x, y, branch, angle))%>%
-  #     tidyr::pivot_longer(-label)
-  # })
+  gandT <-reactive({
+    dataWithSelection()%>%
+      dplyr::mutate_if(is.numeric,as.character, is.factor, as.character) %>%
+      na.omit() %>%
+      dplyr::select(-c(parent, node, branch.length, isTip, x, y, branch, angle))%>%
+      tidyr::pivot_longer(-label)
+  })
+  
+  output$selectedIndivs <- renderTable({ #renderTable makes a table of values - can this be accessed 
+    gandT()
+    #ifelse(dataWithSelection()$isTip == TRUE, dataWithSelection()$label, "") 
+  })
   
   return(make_tree)
 }
