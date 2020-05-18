f20c117716a782fd7edb2141f779f6102a0fe04e
jennahamlin <jennahamlin@gmail.com>
2020-05-18 10:40:59 -0400

    clade annotator talks between snpMean and the plt but output is NaN - meaning it is not grabbing and calculating

--- R/mod_cladeAnnotator.R
+++ R/mod_cladeAnnotator.R
@@ -19,36 +19,36 @@ mod_cladeAnnotator_ui <- function(id){
     tableOutput(ns("contents"))
   )
 }
-    
+
 #' cladeAnnotator Server Function
 #'
 #' @noRd 
 mod_cladeAnnotator_server <- function(input, output, session, make_treeOut, geneObjectOut){
   ns <- session$ns
- 
-
+  
+  
   # #convert to long data frame - three columns. This takes as input the genetic distance object from display tree module 
-   geneFile <-  reactive({
-     geneObjectOut()%>%
-     na.omit()%>%
-     tidyr::pivot_longer(-label)
-   })
-   
-   #remove self comparisons for this table - necessary for snp mean/median calculation. 
-   geneFileCheck <- reactive({
-     geneFile()[which(geneFile()$label != geneFile()$name),]
-   })
-
-   output$contents <- renderTable({
-     geneFileCheck()
-   })
-   
+  geneFile <-  reactive({
+    geneObjectOut()%>%
+      na.omit()%>%
+      tidyr::pivot_longer(-label)
+  })
+  
+  #remove self comparisons for this table - necessary for snp mean/median calculation. 
+  geneFileSNP <- reactive({
+    geneFile()[which(geneFile()$label != geneFile()$name),]
+  })
+  
+  output$contents <- renderTable({
+    geneFileSNP()
+  })
+  
   #displays the tree plot, uses output from the displayTree module 
   observeEvent(input$add_tree, {output$treeDisplay <- renderPlot({
     make_treeOut()})
   })
   
-  # Initialize a reactive value and set to zero for annotations
+  # Initialize a reactive value and set to zero for annotations - this is for updating brush tips selection
   n_annotations <- reactiveVal(0)
   annotations <- reactiveValues()
   
@@ -57,9 +57,10 @@ mod_cladeAnnotator_server <- function(input, output, session, make_treeOut, gene
     brushedPoints(make_treeOut()$data, input$plot_brush)
   })
   
-  #add to label to vector if isTip == True this is necessary to exclude the NA in the tip vector
+  tipVector <- c()
+  
+  #add label to vector if isTip == True this is necessary to exclude the NA in the tip vector associated with nodes
   dataWithSelection2 <- eventReactive(input$plot_brush, {
-    tipVector <- c()
     for (i in 1:length(dataWithSelection()$label)) {
       if (dataWithSelection()$isTip[i] == TRUE)
         tipVector <- c(tipVector, dataWithSelection()$label[i])
@@ -76,10 +77,12 @@ mod_cladeAnnotator_server <- function(input, output, session, make_treeOut, gene
       offset = max(make_treeOut()$data$x)
     )
   }
+  
   snpVector <- c()
   
-   snp_anno <- function(geneFile, tips){ 
-     for (i in 1:length(tips)){
+  #functons which calculates mean snp differences for selected tips
+  snp_anno <- function(geneFile, tips){ 
+    for (i in 1:length(tips)){
       for (j in 1:length(tips)){
         if(tips[i] == tips[j]) next #https://stackoverflow.com/questions/36329183/exclude-one-fixed-variable-in-for-loop
         snpVector[i]<- geneFile%>%
@@ -90,12 +93,13 @@ mod_cladeAnnotator_server <- function(input, output, session, make_treeOut, gene
     return(as.numeric(snpVector))
   }
 
-    snpMean <- eventReactive(input$add_annotation, {lapply(1:n_annotations(), function(i)
-      snp_anno(geneFile = geneFile4(),
-               tips= tip_vector[[i]]))
+    snpMean <- eventReactive(input$add_annotation, {
+                             lapply(1:n_annotations(), function(i)
+      snp_anno(geneFile = geneFileSNP(),
+               tips = dataWithSelection2()[[i]]))
+                 #annotations[[paste0("ann", n_annotations())]] <- dataWithSelection2()))
+
     })
-    
-   
    
   #display that layer onto the tree
   anno_plot <- eventReactive(input$add_annotation, {
@@ -104,16 +108,23 @@ mod_cladeAnnotator_server <- function(input, output, session, make_treeOut, gene
     n_annotations(new)
     #add the tip vector (aka label) to the annotation reactive value
     annotations[[paste0("ann", n_annotations())]] <- dataWithSelection2()
+    #str(annotations[[paste0("ann", n_annotations())]] <- dataWithSelection2())
     
-      
-     
+
+    # snpMean <-
+    #   lapply(1:n_annotations(), function(i)
+    #     snp_anno(geneFile = geneFileSNP(),
+    #              tips =  dataWithSelection2()[[i]]
+    #                #annotations[[paste0("ann", i)]]
+    #                ))
+
     #list apply over the make_layer function to add the annotation
     plt <-
       lapply(1:n_annotations(), function(i)
         make_layer(
           make_treeOut(),
           tips = annotations[[paste0("ann", i)]],
-          label = paste("Clade", "\nSNP Differences"),
+          label = paste("Clade", "\nSNP Differences", lapply(snpMean()[i], mean)),
           color = "red", 
           offset = tipVector[[i]] #can be make_layer
         ))
@@ -148,7 +159,6 @@ mod_cladeAnnotator_server <- function(input, output, session, make_treeOut, gene
     return(plt)
   })
   
-  
   #remove the annotations 
   observeEvent(input$tree_reset,{
     output$treeDisplay <- renderPlot({
@@ -167,10 +177,10 @@ mod_cladeAnnotator_server <- function(input, output, session, make_treeOut, gene
   #reactive to send tree with annoations to downloadImage module 
   treeWLayers <- reactive ({make_treeOut() + anno_plot()})
 }
-    
+
 ## To be copied in the UI
 # mod_cladeAnnotator_ui("cladeAnnotator_ui_1")
-    
+
 ## To be copied in the server
 # callModule(mod_cladeAnnotator_server, "cladeAnnotator_ui_1")
- 
+
