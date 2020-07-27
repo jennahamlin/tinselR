9a17168938d368b5f516c1d1797171091b5c34f1
jennahamlin <jennahamlin@gmail.com>
2020-07-27 14:00:57 -0400

    documented and commited dataset - first test

---
+++ R/tree1504MLEXH.data.R
@@ -0,0 +1,13 @@
+#' Newick formatted phylogenetic tree
+#'
+#' Data - need more of a description
+#'
+#' @docType data
+#'
+#' @usage data(tree1504MLEXH-1)
+#'
+#' @format An object of class `"cross"`
+#'
+#' @keywords datasets
+#'
+#'"tree1504MLEXH-1"
