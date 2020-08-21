f4b0017320e82797420af5ccbf824df47c9bf8bb
Jenna Hamlin <jennahamlin@gmail.com>
2020-08-21 15:17:26 -0400

    attempt to see if example data is usable on a seperate machine

--- R/mod_exampleData.R
+++ R/mod_exampleData.R
@@ -57,11 +57,14 @@ mod_exampleData_server <- function(input, output, session){
   
   exTreeFileUp <- eventReactive(input$exTreeFile, {
     if(input$exTreeFile == "example Tree 1"){
-      exTreeFileUp <-  treeio::read.newick(here::here("inst", "extdata", "1504MLEXH-1.dnd"))
+      Tinsel::tree
+      #load("data-raw/treeSet1.rda")
+      #exTreeFileUp <-  treeio::read.newick(
+        #here::here("inst", "extdata", "1504MLEXH-1.dnd"))
     }
-    else if (input$exTreeFile == "example Tree 2") {
-      exTreeFileUp <-  treeio::read.newick(here::here("inst", "extdata", "1509MNJJP-1_RAxML_bipartitions"))}
-    }
+    # else if (input$exTreeFile == "example Tree 2") {
+    #   exTreeFileUp <-  treeio::read.newick(here::here("inst", "extdata", "1509MNJJP-1_RAxML_bipartitions"))}
+     }
   )
   
   return(
--- R/treeSet1.R
+++ R/treeSet1.R
@@ -1,3 +1,3 @@
 ## code to prepare `treeSet1` dataset goes here
 
-usethis::use_data("treeSet1")
+#usethis::use_data("treeSet1")
