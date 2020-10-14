de1cb2516fd62719047973d45bc49b2bc739ac89
jennahamlin <jennahamlin@gmail.com>
2020-10-14 15:26:46 -0400

    to include

--- R/app_server.R
+++ R/app_server.R
@@ -3,6 +3,10 @@
 app_server <- function(input, output,session) {
   # List the first level callModules here
   
+  ####################
+  #### USERS DATA ####
+  ####################
+  
   #uplodad the data (tree, genetic distance and meta data) and call that module output by dataDisplay$
   dataDisplay <- callModule(mod_uploadData_server, "uploadData_ui_1")
   
@@ -26,32 +30,36 @@ app_server <- function(input, output,session) {
   #callModule(mod_addMatrix_server, "addMatrix_ui_1", buttons$addMatrix, plot$makeTreeOut, dataDisplay$metaFileOut, dataDisplay$metaSep)
   
   #annotates tree with incorporated tree viz parameters
-  treeWLayers <- callModule(mod_cladeAnnotator_server, "cladeAnnotator_ui_data",dataDisplay$metaFileOut, dataDisplay$metaSep, plot$makeTreeOut,
-                            buttons$addTree, buttons$addAnno, buttons$removeAnno,  buttons$addMatrix, dataDisplay$geneObjectForSNP, params$labelOff, 
-                            params$labColor)
-  
-  #params$overlapAd - add if i can figure this out 
+  treeWLayers <- callModule(mod_cladeAnnotator_server, "cladeAnnotator_ui_data", dataDisplay$metaFileOut, dataDisplay$metaSep, plot$makeTreeOut,
+                            buttons$addTree, buttons$addAnno, buttons$removeAnno, buttons$addMatrix, dataDisplay$geneObjectForSNP, params$labelOff, 
+                            params$labColor, params$matOff)
   
   #allows tree with annotation and viz parameters to be donwloaded
   callModule(mod_downloadImage_server, "downloadImage_ui_1", treeWLayers)
   
-  ##repeated for preloaded data; no need to download so that module is not included below
-  #uplodad the data (tree, genetic, and meta) and call that module dataDisplay. This is the only module that is re-written, the rest of the same 
-  #used in the above section and are called the same as above. 
-  # exampleData <- callModule(mod_exampleData_server, "exampleData_ui_1")
-  # 
-  # exampleParams <- callModule(mod_paramsTree_server, "paramsTree_ui_example")
-  # 
-  # exampleButtons <- callModule(mod_pushButtons_server, "pushButtons_ui_example")
-  # 
-  # callModule(mod_relaunchApp_server, "relaunchApp_ui_example")
-  # 
-  # examplePlot <- callModule(mod_displayTree_server, "displayTree_ui_example", exampleData$extreeFileOut, exampleData$exGeneFileCorOrUnOut,
-  #           exampleParams$align, exampleParams$treeformat, exampleParams$font, exampleParams$numscale, exampleParams$node, exampleParams$lim,   
-  #           exampleParams$bootPos, exampleParams$midP)
-  # 
-  # callModule(mod_cladeAnnotator_server, "cladeAnnotator_ui_example", examplePlot$makeTreeOut, exampleButtons$addTree, 
-  #            exampleButtons$addAnno, exampleButtons$removeAnno, exampleData$exGeneObjectForSNP,  exampleParams$labelOff, exampleParams$labColor)
-  # 
+  ####################
+  ### EXAMPLE DATA ###
+  ####################
+  
+  #repeated for preloaded data; no need to download so that module is not included below
+  #uplodad the data (tree, genetic, and meta) and call that module dataDisplay. This is the only module that is re-written, the rest of the same
+  #used in the above section and are called the same as above.
+  
+  exampleData <- callModule(mod_exampleData_server, "exampleData_ui_1")
+
+  exampleParams <- callModule(mod_paramsTree_server, "paramsTree_ui_example")
+
+  exampleButtons <- callModule(mod_pushButtons_server, "pushButtons_ui_example")
+
+  callModule(mod_relaunchApp_server, "relaunchApp_ui_example")
+
+  examplePlot <- callModule(mod_displayTree_server, "displayTree_ui_example", exampleData$extreeFileOut, exampleData$exGeneFileCorOrUnOut,
+            exampleParams$align, exampleParams$treeformat, exampleParams$font, exampleParams$numscale, exampleParams$node, exampleParams$lim,
+            exampleParams$bootPos, exampleParams$midP)
+
+  callModule(mod_cladeAnnotator_server, "cladeAnnotator_ui_example",exampleData$metaFileOut, exampleData$metaSep, examplePlot$makeTreeOut, 
+             exampleButtons$addTree, exampleButtons$addAnno, exampleButtons$removeAnno, exampleButtons$addMatrix, exampleData$exGeneObjectForSNP,
+             exampleParams$labelOff, exampleParams$labColor, exampleParams$matOff)
+
 }
 
