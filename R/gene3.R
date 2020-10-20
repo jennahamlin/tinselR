efbe24957b4463ff58d8fb17cef46cdd16b6d1f8
Jenna <jennahamlin@gmail.com>
2020-10-20 12:11:55 -0400

    add for previous comment of better documentation for gene3 dataset

--- R/gene3.R
+++ R/gene3.R
@@ -1,6 +1,23 @@
-#' @title example gene 3
+#' @title Example Gene 3
+#' 
+#'This is a genetic distance matrix of SNPs for 19 isolates of Salmonella 
+#'enterica to be used with the newick phylogenetic tree (example tree 3) and 
+#'the optional meta data file (example meta 3)
+#'
 #' @docType data
+#' 
 #' @rdname gene3
 #'
 #' @usage data(gene3)
+#' 
+#' @keywords datasets
+#' 
+#' #' @examples
+#' data(gene3)
+#' #convert to three column tibble by  label column and remove self comparisons
+#' gene3 <- gene3 %>% dplyr::rename( label = 1)%>%
+#'  tidyr::pivot_longer(-label)%>%
+#'  .[which(.$label != .$name),]%>%
+#'  dplyr::mutate(value=ifelse(value=="-", 0,value))
+#' 
 "gene3"
