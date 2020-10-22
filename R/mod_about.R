504460e99f428134b8a6892b60c1644b3f4d7292
Jenna <jennahamlin@gmail.com>
2020-10-22 12:26:42 -0400

    added pics for about page and update that page

--- R/mod_about.R
+++ R/mod_about.R
@@ -14,27 +14,22 @@
 mod_about_ui <- function(id) {
   ns <- NS(id)
   tagList(
-    fluidRow(column(12, offset = 0, style = "padding:0px;",
+    fluidRow(column(12, offset = 0,
                     mainPanel(h1(strong("Getting started with Tinsel")),
-                              br(),
+                              tags$br(),
                               p("Tinsel at its' most basic level is a graphical
                               viewer of newick phylogenetic trees and as a
                               program for producing publication-ready figures.",
-                                br(),
-                                "The", em("power"),"of Tinsel comes with
-                                combining a genetic distance matrix for
-                                annotating a tree for epidemological
-                                outbreak analyses.",
-                                br(),
-                                "A genetic distance matrix contains the
-                              number of snp differences for all pairwise
-                              comparisons.", br(), "Note that the data you
-                              upload is only held within the application while
-                              on the site (i.e. no data is stored.)"),
-                              br(),
-                              strong("Please click on the 'Data Upload' pane to
-                                     upload your files."),
-                              br(),
+                                "The", em("power"),"of Tinsel comes with combining
+                              a genetic distance matrix for annotating a tree
+                              for epidemological outbreak analyses.", "A genetic
+                              distance matrix contains the number of snp 
+                              differences for all pairwise comparisons. Note 
+                              that the data you upload is only held within the
+                              application while on the site (i.e. no data is 
+                              stored.)"),
+                              tags$strong("Please click on the 'Data Upload' 
+                                          pane to upload your files."),
                               tags$li(em("Phylogenetic Tree"), "- required; a",
                                       a("newick",
                                         href =
@@ -42,36 +37,52 @@ mod_about_ui <- function(id) {
                                         Newick_format"
                                       ),
                                       "generated tree"),
+                              tags$br(),
                               tags$li(em("Genetic Distance Data"),
                                       "- optional for use with the annotation
-                                      function;",
-                                      br(), "a tsv/txt/csv file of
-                                      SNP differences, requires column headers"
-                              ),
+                                      function.",
+                                      tags$br(), "A tsv/txt/csv file of
+                                      SNP differences. See image below for 
+                                      example"),
+                              tags$br(),
+                              tags$img(src = "www/geneDistanceExample.PNG",
+                                       height = "50%", width = "50%"),
+                              tags$br(),
                               tags$li(em("Meta Data"), "- optional for easy
                                       correction of tip labels;",
-                                      br(), "a tsv/txt/csv file - requires
+                                      tags$br(), "a tsv/txt/csv file - requires
                                       column headers of Display.labels and
-                                      Tip.labels"),
-                              br(), 
-                              strong("Once the phylogenetic tree is uploaded
-                                     you can -"),
-                              br(),
-                              "Alter additional visualization parameters",
-                              br(),
+                                      Tip.labels. See image Below for 
+                                      example."),
+                              tags$br(),
+                              tags$img(src = "www/metaDataExample.PNG",
+                                       height = "50%", width = "50%"),
+                              tags$br(), 
+                              tags$li(tags$strong("Once the phylogenetic tree is
+                              uploaded you can -")),
+                              tags$br(),
+                              "Alter visualization parameters",
+                              tags$br(),
+                              tags$img(src = "www/treeWAlignedTips.PNG",
+                                       height = "50%", width = "50%"),
+                              tags$br(), 
                               strong("Once the genetic distance file is
                                      uploaded you can -"),
-                              br(),
                               "add annotation to the visual representation of
                               the tree",
-                              br(),
+                              tags$br(),
+                              tags$img(src = "www/treeWAnno.PNG",
+                                       height = "50%", width = "50%"),
+                              tags$br(), 
+                              
+                              tags$br(),
                               strong("When you are happy with the way your tree
                                      looks-"),
-                              br(),
+                              tags$br(),
                               "scroll to the bottom; enter your name and press
                               Download",
-                              br(),
-                              br(),
+                              tags$br(),
+                              tags$br(),
                               "If you have any problems, please file an",
                               a("issue", href =
                                   "https://github.com/jennahamlin/Tinsel/issues"
