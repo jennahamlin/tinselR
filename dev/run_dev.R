d22282b6a21cbad568f8efb4d7b726da5e5eb6dd
Jenna <jennahamlin@gmail.com>
2020-11-15 16:19:48 -0500

    removed extra line

--- dev/run_dev.R
+++ dev/run_dev.R
@@ -4,7 +4,6 @@ options(golem.app.prod = FALSE)
 # Detach all loaded packages and clean your environment
 golem::detach_all_attached()
 
-
 # Document and reload your package
 golem::document_and_reload()
 
