3448fb92104efa53524c11d5421824c548a12a1d
Jenna <jennahamlin@gmail.com>
2020-11-18 16:49:20 -0500

    switch back of varaible name

--- R/golem_utils_server.R
+++ R/golem_utils_server.R
@@ -76,7 +76,7 @@ not_columns <- function(file) {
 
 #change column1, row1 to the id of label.The input parameter is the genetic
 #distance file. Necessary for downstream steps
-replace_column_header <- function(g_file) {
+replace_column_header <- function(gene_file_in) {
   . <- NULL
   dplyr::rename(gene_file_in, label = 1)
   #rename column 1 to label for joining of data sets later
