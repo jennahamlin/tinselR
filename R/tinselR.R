ba53bdbb4150056633df142c341ff128a5fb4558
Jenna <jennahamlin@gmail.com>
2020-11-18 12:50:19 -0500

    test to see what help page looks like

--- R/tinselR.R
+++ R/tinselR.R
@@ -1,12 +1,13 @@
 #' tinselR: An R Shiny Application for annotating outbreak phylogenetic trees
 #'
-#' This is a phylogenetic tree application for adding additional information to
-#' trees. Additional information could come from a genetic distance file or
+#' This is a phylogenetic tree application for annotating trees with additional
+#' information. Additional information could come from a genetic distance file or
 #' meta data file.
 #' 
 #' 
-#' @section tinselR functions:
-#' The tinselR functions ...
+#' @section tinselR modules:
+#' The main modules of tinselR include:
+#' mod_about - 
 #'
 #' @docType package
 #' @name tinselR
